3fc0b3d283a8de8e65e25bb385e1af48a5a92d25
Sushi6006 <lin.terry.x2@gmail.com>
2020-11-08 15:55:52 +1100

    rebuilt

--- Assets/Scripts/Menu Script/MenuButton.cs
+++ Assets/Scripts/Menu Script/MenuButton.cs
@@ -15,11 +15,6 @@ public class MenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     [SerializeField] int thisIndex;
     [SerializeField] bool mouse_over = false;
 
-
-
-    
-
-
     void Start()
     {
         
